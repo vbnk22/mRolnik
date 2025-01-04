8d9c187f28789df59de71c4bdb26aeaef9966b79
ASton <313280@office.umcs.pl>
2025-01-04 22:24:44 +0100

    Implementation of a class for logging in and registration

---
+++ CODE/app/src/main/java/com/example/mrolnik/classes/User.kt
@@ -0,0 +1,11 @@
+package com.example.mrolnik.classes
+
+class User {
+    var userId: Int = 0
+    lateinit var firstName: String
+    lateinit var lastName: String
+    lateinit var login: String
+    lateinit var password: String
+    var plannerId: Int = 0
+    var systemId: Int = 0
+}
